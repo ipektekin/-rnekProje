5225bdaa51c05d3418b7be0c09fd5c1d0b3caed5
Tuna Vanli <tunavanli@gmail.com>
2023-02-01 10:35:22 +0300

    Tek basamaklı toplama işlemi ekledi

--- ÖrnekProje/Form1.cs
+++ ÖrnekProje/Form1.cs
@@ -35,6 +35,47 @@ namespace ÖrnekProje
             iki = 2;
         }
 
+        int üç;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            üç = 3;
+        }
+
+        int dört;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dört = 4;
+        }
+
+        int beş;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            beş = 5;
+        }
+
+        int altı;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            altı = 6;
+        }
+
+        int yedi;
+        private void button7_Click(object sender, EventArgs e)
+        {
+            yedi = 7;
+        }
+
+        int sekiz;
+        private void button8_Click(object sender, EventArgs e)
+        {
+            sekiz = 8;
+        }
+
+        int dokuz;
+        private void button9_Click(object sender, EventArgs e)
+        {
+            dokuz = 9;
+        }
         string işlem ;
         private void ToplaButon_Click(object sender, EventArgs e)
         {
@@ -45,8 +86,21 @@ namespace ÖrnekProje
         {
             if(işlem == "topla")
             {
-                sonuctextbox.Text = Convert.ToString(bir + iki);
+                int toplam = bir + iki + üç + dört + beş + altı + yedi + sekiz + dokuz;
+                sonuctextbox.Text = Convert.ToString(toplam);
             }
         }
-    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SıfırlaButon_Click(object sender, EventArgs e)
+        {
+            bir = 0; iki = 0; üç = 0; dört = 0; beş = 0; altı = 0; yedi = 0; sekiz = 0; dokuz = 0;
+            işlem = "";
+            sonuctextbox.Text = "";
+        }
+    
 }
